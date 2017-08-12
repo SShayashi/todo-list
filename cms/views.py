29a439b8b1d4e25d50ab1e9c3157396387a82418
hayashi <sshayashi@star-prince.comsshayashi@star-prince.comsshayashi@star-prince.com>
2017-08-12 14:58:56 +0900

    add empty method in views.py

--- cms/views.py
+++ cms/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def task_list(request):
+    """タスク一覧"""
+    return HttpResponse('タスクの一覧')
+
+
+def task_edit(request, book_id=None):
+    """タスクの編集"""
+    return HttpResponse('タスクの編集')
+
+
+def task_delete(request, book_id):
+    """タスクの削除"""
+    return HttpResponse('タスクの削除')
