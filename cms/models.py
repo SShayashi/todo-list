5c623d977a9ca55b37bff79dca32768556b48d12
hayashi <sshayashi@star-prince.comsshayashi@star-prince.comsshayashi@star-prince.com>
2017-08-12 14:59:10 +0900

    Fix models

--- cms/models.py
+++ cms/models.py
@@ -3,20 +3,19 @@ from django.db import models
 # Create your models here.
 
 
-class Book(models.Model):
-    """書籍"""
-    name = models.CharField('書籍名', max_length=255)
-    publisher = models.CharField('出版社', max_length=255, blank=True)
-    page = models.IntegerField('ページ数', blank=True, default=0)
+class Task(models.Model):
+    """Task"""
+    name = models.CharField('TaskName', max_length=255)
+    content = models.CharField('TaskContent', max_length=255)
 
     def __str__(self):
         return self.name
 
 
-class Impression(models.Model):
-    """感想"""
-    book = models.ForeignKey(Book, verbose_name='書籍', related_name='impressions')
-    comment = models.TextField('コメント', blank=True)
+class Comment(models.Model):
+    """Comment"""
+    book = models.ForeignKey(Task, verbose_name='Task', related_name='Comments')
+    comment = models.TextField('comment', blank=True)
 
     def __str__(self):
         return self.comment
