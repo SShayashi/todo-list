30a223888da059793a5a74ada13bfcbda6505bf3
hayashi <sshayashi@star-prince.comsshayashi@star-prince.comsshayashi@star-prince.com>
2017-08-12 22:15:50 +0900

    fix routing

--- cms/urls.py
+++ cms/urls.py
@@ -4,7 +4,7 @@ from cms import views
 urlpatterns = [
     # 書籍
     url(r'^task/$', views.task_list, name='task_list'),
-    url(r'^task/add/$', views.task_add, name='task_add'),
+    url(r'^task/add/$', views.task_edit, name='task_add'),
     url(r'^task/mod/(?P<task_id>\d+)/$', views.task_edit, name='task_mod'),
     url(r'^task/del/(?P<task_id>\d+)/$', views.task_delete, name='task_del'),
 ]
