a1ec7da0c81842208806e3e9ba7d8780577d6e37
hayashi <sshayashi0208@gmail.com>
2017-08-13 18:55:37 +0900

    Add new model ‘List’

--- cms/admin.py
+++ cms/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from cms.models import Task, Comment
+from cms.models import List, Task, Comment
 
+admin.site.register(List)
 admin.site.register(Task)
 admin.site.register(Comment)
--- cms/models.py
+++ cms/models.py
@@ -3,10 +3,19 @@ from django.db import models
 # Create your models here.
 
 
+class List(models.Model):
+    name = models.CharField('ListName', max_length=255, default='NewList')
+
+    def __str__(self):
+        return self.name
+
+
 class Task(models.Model):
     """Task"""
+    list = models.ForeignKey(List, verbose_name='List', related_name='Tasks')
     name = models.CharField('TaskName', max_length=255)
     content = models.CharField('TaskContent', max_length=255)
+    is_completed = models.BooleanField('is_completed', default=False)
 
     def __str__(self):
         return self.name
