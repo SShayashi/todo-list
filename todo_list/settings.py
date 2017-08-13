ca36085222ad063af9a71137b180c9c4cf957f17
hayashi <sshayashi@star-prince.comsshayashi@star-prince.comsshayashi@star-prince.com>
2017-08-13 13:37:03 +0900

    Change database from sqlite to mysql

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -79,11 +79,14 @@ WSGI_APPLICATION = 'todo_list.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'zzz',
+        'USER': 'root',
+        'PASSWORD': 'zzz',
+        'HOST': '0.0.0.0',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
